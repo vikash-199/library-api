804be9289b5110a1abebcd17015faf53721a5a70
vikash kumar <vikashkumar24041997@gmail.com>
2025-11-07 01:36:25 +0530

    setput code

--- config/db.js
+++ config/db.js
@@ -3,9 +3,9 @@ import mongoose from "mongoose";
 export const connectDB = async () => {
   try {
     await mongoose.connect(process.env.MONGO_URI);
-    console.log("✅ MongoDB Connected");
+    console.log(" MongoDB Connected");
   } catch (error) {
-    console.error("❌ DB Connection Error:", error.message);
+    console.error(" DB Connection Error:", error.message);
     process.exit(1);
   }
 };
--- server.js
+++ server.js
@@ -6,4 +6,4 @@ dotenv.config();
 connectDB();
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
+app.listen(PORT, () => console.log(` Server running on port ${PORT}`));
